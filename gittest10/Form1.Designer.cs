7d10f351df2e91c63971c1a7cd1a2f957a47d08d
qiaojie <qiaojienj@163.com>
2020-10-21 11:15:50 +0800

    提交

--- gittest10/Form1.Designer.cs
+++ gittest10/Form1.Designer.cs
@@ -31,6 +31,7 @@
       this.components = new System.ComponentModel.Container();
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.Text = "Form1";
+      //暂存
     }
 
     #endregion
