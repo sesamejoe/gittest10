6fc7aab9c03802bd41e61f0d8774b814ba452f38
qiaojie <qiaojienj@163.com>
2020-10-21 10:49:12 +0800

    还原2

--- gittest10/Form1.Designer.cs
+++ gittest10/Form1.Designer.cs
@@ -28,46 +28,12 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.SuspendLayout();
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(64, 55);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(55, 15);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "label1";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(67, 104);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(55, 15);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "label2";
-      // 
-      // Form1
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+      this.components = new System.ComponentModel.Container();
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(282, 253);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.label1);
-      this.Name = "Form1";
       this.Text = "Form1";
-      this.ResumeLayout(false);
-      this.PerformLayout();
-
     }
 
     #endregion
-
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label label2;
   }
 }
 
